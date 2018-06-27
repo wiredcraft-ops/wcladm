28b76f08f240c4ddfe32348b063916bd0fb9ea40
Kaleo Cheng <kaleocheng@gmail.com>
2018-06-27 16:39:56 +0800

    Add --dest flag for init harbor

--- cmd/harbor.go
+++ cmd/harbor.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"text/template"
 
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/wiredcraft-ops/wcladm/templates"
 	"github.com/wiredcraft-ops/wcladm/types"
@@ -21,6 +23,10 @@ type harborTemplate struct {
 }
 
 var (
+	dest string
+)
+
+var (
 	harborConfig = &types.HarborConfiguration{
 		HostName:      "registry.wiredcraft.cn",
 		Scheme:        "https",
@@ -28,21 +34,6 @@ var (
 		SSLCertKey:    "/tmp/certs",
 		AdminPassword: "12345",
 		DBPassword:    "12345",
-	}
-
-	harborTemplates = []harborTemplate{
-		{"/tmp/wcladm-harbor/common/templates/nginx/nginx.https.conf", templates.HarborNginxConfigTempl, nil},
-		{"/tmp/wcladm-harbor/harbor.cfg", templates.HarborConfigTempl, harborConfig},
-		{"/tmp/wcladm-harbor/prepare", templates.HarborPrepareTempl, nil},
-		{"/tmp/wcladm-harbor/docker-compose.yml", templates.HarborDockerComposeTempl, nil},
-		{"/tmp/wcladm-harbor/common/templates/adminserver/env", templates.HarborAdminServerEnvTempl, nil},
-		{"/tmp/wcladm-harbor/common/templates/db/env", templates.HarborDBEnvTempl, nil},
-		{"/tmp/wcladm-harbor/common/templates/jobservice/config.yml", templates.HarborJobServerConfigTempl, nil},
-		{"/tmp/wcladm-harbor/common/templates/jobservice/env", templates.HarborJobServerEnvTempl, nil},
-		{"/tmp/wcladm-harbor/common/templates/log/logrotate.conf", templates.HarborLogrotateConfigTempl, nil},
-		{"/tmp/wcladm-harbor/common/templates/registry/config.yml", templates.HarborRegistryConfigTempl, nil},
-		{"/tmp/wcladm-harbor/common/templates/ui/app.conf", templates.HarborUIAppTempl, nil},
-		{"/tmp/wcladm-harbor/common/templates/ui/env", templates.HarborUIEnvTempl, nil},
 	}
 )
 
@@ -57,13 +48,22 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+
+		dir, err := homedir.Expand(dest)
+		if err != nil {
+			fmt.Println("Cannot get your harbor dest dir")
+			os.Exit(1)
+		}
+
+		harborTemplates := getTemplates(dir)
+
 		fmt.Println("Rendering harbor templates...")
 		writeTemplateFile(harborTemplates)
 
 		fmt.Println("Generating the configuration...")
 
 		var pstdout, pstderr bytes.Buffer
-		prepare := exec.Command("python", "/tmp/wcladm-harbor/prepare")
+		prepare := exec.Command("python", path.Join(dir, "prepare"))
 		prepare.Stdout = &pstdout
 		prepare.Stderr = &pstderr
 		if err := prepare.Run(); err != nil {
@@ -75,7 +75,7 @@ to quickly create a Cobra application.`,
 
 		fmt.Println("Starting harbor...")
 		var cstdout, cstderr bytes.Buffer
-		compose := exec.Command("docker-compose", "-f", "/tmp/wcladm-harbor/docker-compose.yml", "up", "-d")
+		compose := exec.Command("docker-compose", "-f", path.Join(dir, "docker-compose.yml"), "up", "-d")
 		compose.Stdout = &cstdout
 		compose.Stderr = &cstderr
 		if err := compose.Run(); err != nil {
@@ -89,6 +89,7 @@ to quickly create a Cobra application.`,
 
 func init() {
 	initCmd.AddCommand(harborCmd)
+	harborCmd.Flags().StringVarP(&dest, "dest", "d", "~/.wcladm/harbor", "harbor dest")
 }
 
 func writeTemplateFile(ts []harborTemplate) {
@@ -121,4 +122,26 @@ func mkdir(dir string) error {
 		}
 	}
 	return nil
+}
+
+func getTemplates(dir string) []harborTemplate {
+
+	h := []harborTemplate{
+		{"harbor.cfg", templates.HarborConfigTempl, harborConfig},
+		{"prepare", templates.HarborPrepareTempl, nil},
+		{"docker-compose.yml", templates.HarborDockerComposeTempl, nil},
+		{"common/templates/adminserver/env", templates.HarborAdminServerEnvTempl, nil},
+		{"common/templates/db/env", templates.HarborDBEnvTempl, nil},
+		{"common/templates/nginx/nginx.https.conf", templates.HarborNginxConfigTempl, nil},
+		{"common/templates/jobservice/config.yml", templates.HarborJobServerConfigTempl, nil},
+		{"common/templates/jobservice/env", templates.HarborJobServerEnvTempl, nil},
+		{"common/templates/log/logrotate.conf", templates.HarborLogrotateConfigTempl, nil},
+		{"common/templates/registry/config.yml", templates.HarborRegistryConfigTempl, nil},
+		{"common/templates/ui/app.conf", templates.HarborUIAppTempl, nil},
+		{"common/templates/ui/env", templates.HarborUIEnvTempl, nil},
+	}
+	for i := range h {
+		h[i].Path = path.Join(dir, h[i].Path)
+	}
+	return h
 }
