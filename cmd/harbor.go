fb6c3304580b5ce181cf9e9d254d3434f7bee89a
Kaleo Cheng <kaleocheng@gmail.com>
2018-06-27 12:38:32 +0800

    Add writeTemplateFile for init harbor

--- cmd/harbor.go
+++ cmd/harbor.go
@@ -3,20 +3,33 @@ package cmd
 import (
 	"log"
 	"os"
+	"text/template"
 
 	"github.com/spf13/cobra"
 	"github.com/wiredcraft-ops/wcladm/templates"
 	"github.com/wiredcraft-ops/wcladm/types"
 )
 
-var harborConfig = &types.HarborConfiguration{
-	HostName:      "registry.wiredcraft.cn",
-	Scheme:        "https",
-	SSLCert:       "/tmp/certs",
-	SSLCertKey:    "/tmp/certs",
-	AdminPassword: "12345",
-	DBPassword:    "12345",
+type harborTemplate struct {
+	Path     string
+	Template *template.Template
+	Data     interface{}
 }
+
+var (
+	harborConfig = &types.HarborConfiguration{
+		HostName:      "registry.wiredcraft.cn",
+		Scheme:        "https",
+		SSLCert:       "/tmp/certs",
+		SSLCertKey:    "/tmp/certs",
+		AdminPassword: "12345",
+		DBPassword:    "12345",
+	}
+
+	harborTemplates = []harborTemplate{
+		{"common/templates/nginx/nginx.conf", templates.HarborNginxConfigTempl, nil},
+	}
+)
 
 // harborCmd represents the harbor command
 var harborCmd = &cobra.Command{
@@ -42,4 +55,21 @@ to quickly create a Cobra application.`,
 
 func init() {
 	initCmd.AddCommand(harborCmd)
+}
+
+func writeTemplateFile(ts []harborTemplate) {
+	for _, t := range ts {
+		f, err := os.Create(t.Path)
+		if err != nil {
+			log.Println("create file: ", err)
+			return
+		}
+
+		err = t.Template.Execute(f, t.Data)
+		if err != nil {
+			log.Print("execute: ", err)
+			return
+		}
+		f.Close()
+	}
 }
